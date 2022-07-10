83fc99bd81a6c8e5016ee74a6a672015cfa310df
Vikash Choubey <apple@Choubs-MacBook-Pro.local>
2022-07-10 11:13:50 +0530

    Added support for etherum

--- src/start_setup.sh
+++ src/start_setup.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 if [[ "$1" == "-h" ]]; then
-  echo "Usage: sh start_setup.sh <environment_file_path> <service_name>"
-  echo "service_name can be [all, graph-node-indexer, graph-node-query-0, graph-node-query-1, ipfs, database]]"
-  exit 0
+    echo "Usage: sh start_setup.sh <docker-compose-file> <environment_file_path> <service_name>"
+    echo "service names [all, graph-node-indexer, graph-node-query-0, graph-node-query-1, ipfs, database]] for celo"
+    echo "service names [all, eth-graph-node-indexer, eth-graph-node-query-0,
+            eth-graph-node-query-1, eth-ipfs, eth-database]] for etherum"
+    exit 0
 fi
 
-if [[ "$#" -eq 0 ]]
+if [[ "$#" -lt 2 ]]
 then
-  echo "Please provide environment file path"
-  exit 0
+    echo "Please provide docker file and environment file path"
+    exit 0
 fi
 
-echo "Environment file $1"
-. ./"$1"
+echo "Environment file $2"
+. ./"$2"
 
-if [[ "$2" == "all" ]] ||  [[ "$#" -eq 1 ]]; then
-  docker compose up -d --remove-orphans --build
-  exit 0
+if [[ "$3" == "all" ]] || [[ "$#" -eq 1 ]];
+then
+    echo "Starting all services from docker file $1"
+    docker compose -f "$1" up -d --build
+    exit 0
 fi
 
-docker compose up -d --remove-orphans --build $2
+echo "Starting all services from docker file $1, service: $3"
+docker compose -f "$1" up -d --build "$3"
