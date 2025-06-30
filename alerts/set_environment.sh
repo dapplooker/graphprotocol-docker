cc58f2c8d9f6c4af748aacbefd8df8c03f9f55c0
Ankur <ankur@email.com>
2025-06-30 15:35:31 +0530

    updated multi-support env variable

--- alerts/set_environment.sh
+++ alerts/set_environment.sh
@@ -8,11 +8,30 @@ export DISCORD_USER_ID_TAGS=''
 
 # Node monitoring webhook configuration
 export DISCORD_WEBHOOK_URL=''
-export LOCAL_NODE_URL=''
-export PUBLIC_NODE_URL=''
-export NODE_NAME=''
+
+# MULTI-NODE CONFIGURATION:
+# Configure multiple nodes using indexed environment variables
+
+# Node 1 
+export LOCAL_NODE_URL_1=''
+export PUBLIC_NODE_URL_1=''
+export NODE_NAME_1=''
+
+# Node 2 
+export LOCAL_NODE_URL_2=''
+export PUBLIC_NODE_URL_2=''
+export NODE_NAME_2=''
+
+# Node 3
+export LOCAL_NODE_URL_3=''
+export PUBLIC_NODE_URL_3=''
+export NODE_NAME_3=''
+
+# Add more nodes by incrementing the index number (4, 5, 6, etc.)
+
+# Default configuration
 export BLOCK_THRESHOLD=100
 export RETRY_DELAY=300000
 
 # Common variables
-export HOST_NAME=$(hostname) 
+export HOST_NAME=$(hostname)
